35ba1a0129caacc980fb3c4b6be22433fb0888a5
Sam Warren <samwarren4@gmail.com>
2023-02-16 16:16:06 -0800

    move fretArr to constants

--- src/components/StringC.tsx
+++ src/components/StringC.tsx
@@ -1,26 +1,9 @@
 import React from "react";
 import "./StringC.css";
-
+import { fretArr } from "../constants/constants";
 interface stringProps {
   tuningShift: number;
 }
-
-//index = the fret
-//element = the note name   0    1     2    3     4    5    6     7    8     9    10    11
-const fretArr: string[] = [
-  "C",
-  "C#",
-  "D",
-  "Eb",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "Ab",
-  "A",
-  "Bb",
-  "B",
-];
 
 //receives a note name for the current sting and determines the fret number for it
 const getFretNum = (noteName: string) => {
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,3 +5,20 @@ export const tunings: ITuning = {
   dropDTuning: [2, 9, 2, 7, 11, 4],
   openCTuning: [0, 7, 0, 7, 0, 4],
 };
+
+//index = the fret
+//element = the note name
+export const fretArr: string[] = [
+  "C",
+  "C#",
+  "D",
+  "Eb",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "Ab",
+  "A",
+  "Bb",
+  "B",
+];
