d69990515a5115d77e907e36603bdade9f01ec0c
Lenner <Grayson.Lenner@quartech.com>
2023-02-22 13:10:36 -0800

    string color changes when a note button is selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import StringC from './components/StringC'
--- src/components/StringC.tsx
+++ src/components/StringC.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './StringC.css';
+import { useState } from 'react';
 
 interface stringProps{
   tuningShift: number;
@@ -9,15 +10,22 @@ interface stringProps{
 //index = the fret
 //element = the note name   0    1     2    3     4    5    6     7    8     9    10    11
 const fretArr: string[] = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
+const noteButtonColor: string[] = ['black', 'green'];
 
 //receives a note name for the current sting and determines the fret number for it
 const getFretNum = (noteName: string) => {
   return fretArr.indexOf(noteName);
 }
-//retrived the note name at a particular fret num for the current string
-const getNoteName = (fretNum: number) => {
-  return fretArr[fretNum];
+
+
+//gets called when a note button is clicked
+//is responsible to toggling off the last note
+//on the same string, and toggling on the new note
+const swapSelectedNote = (noteName: string) => {
+  const fretNum = getFretNum(noteName);
+ // selected=1;
 }
+
 
 /*
 * returns a rotated version of the original chromatic C scale array
@@ -28,15 +36,26 @@ function rotateStringArray(array: string[], rotations: number): string[] {
 }
 
 function StringC(props: stringProps) {
+  
   let fretArrTuned = fretArr;
   fretArrTuned = rotateStringArray(fretArrTuned, props.tuningShift);
-  //console.log("here");
-  //console.log(fretArr);
+
+  const [selected, setSelected] = useState(false);
 
   return (
     <div className="button-row">
       {fretArrTuned.map((noteName) => (
-        <button className="button" key={noteName} onClick={() => props.onClick(noteName)}>
+        <button 
+          style = {selected ? { backgroundColor: 'green' } : { backgroundColor: 'black' }}
+          className="button" 
+          key={noteName} 
+          onClick={() => {
+            props.onClick(noteName)
+           // swapSelectedNote(noteName);
+            setSelected(!selected);
+          }}
+          
+        >
           {noteName}
         </button>
       ))}
